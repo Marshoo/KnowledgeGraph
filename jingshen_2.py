bed73bbdf13d038cc66d814b0c5b66dbc878f43e
Marshoo <1783851422@qq.com>
2021-06-30 15:41:27 +0800

    第二次提交

--- jingshen_2.py
+++ jingshen_2.py
@@ -14,4 +14,3 @@ emr_list = data_df['emr'].tolist()   # 提取 data_list 中的信息
 emr_df = pd.DataFrame(emr_list)
 df = emr_df.join(pd.DataFrame(emr_df[0].values.tolist()))   # 提取emr_df每行字典中的信息
 df2 = pd.DataFrame(df, columns=['主诉', '现病史', '体格检查', '辅助检查', '诊断', '处理意见', '备注', '签名'])
-
